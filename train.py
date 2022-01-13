72deb71a628d6648e2dc6099deda60c5128a6f6e
Franklin Adjei <57026567+Hotlynn2@users.noreply.github.com>
2022-01-13 22:13:44 +0000

    unneeded change

--- train.py
+++ train.py
@@ -30,7 +30,7 @@ LOAD_MODEL = True
 TRAIN_IMG_DIR = "data/train_images/"
 TRAIN_MASK_DIR = "data/train_masks/"
 VAL_IMG_DIR = "data/val_images/"
-VAL_MASK_DIR = "data/val_masks/a"
+VAL_MASK_DIR = "data/val_masks/"
 
 def train_fn(loader, model, batch_size, optimizer, loss_fn, scaler):
     loop = tqdm(loader)
